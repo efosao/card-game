a0a4e595017bfad4a1bcfce591004966228592b9
Efosa Oyegun <efosao@gmail.com>
2020-05-25 22:05:19 -0700

    implemented card init

--- cypress/integration/card_spec.ts
+++ cypress/integration/card_spec.ts
@@ -4,6 +4,9 @@ import { CardDeck } from "../../src/index";
 describe("Card Deck", () => {
   const deck = new CardDeck();
   it("New deck constains 52 cards", () => {
+    for (let c in deck.cards) {
+      cy.log({ c: deck.cards[c] });
+    }
     expect(get(deck, ["cards", "length"])).to.equal(52);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,37 @@
 export class CardDeck {
-  cards: [];
+  cards: Card[];
 
-  constructor() {}
+  constructor() {
+    this.init();
+  }
+
+  public init() {
+    const cards = [];
+
+    for (let s in Suites) {
+      for (let i = 0; i < 13; i += 1) {
+        cards.push(new Card(i + 1, Suites[s]));
+      }
+    }
+    this.cards = cards;
+  }
+}
+
+export class Card {
+  value: number;
+  suite: Suites;
+
+  constructor(value: number, suite: Suites) {
+    this.suite = suite;
+    this.value = value;
+  }
+}
+
+enum Suites {
+  "clubs" = "Clubs",
+  "Diamonds" = "Diamonds",
+  "Hearts" = "Hearts",
+  "Spades" = "Spades",
 }
 
 function main() {
