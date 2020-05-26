4c542dc17d4865c081b97f37ab9724fcf9f952c1
Efosa Oyegun <efosao@gmail.com>
2020-05-25 22:14:07 -0700

    fixed linting error

--- cypress/integration/card_spec.ts
+++ cypress/integration/card_spec.ts
@@ -5,7 +5,7 @@ describe("Card Deck", () => {
   const deck = new CardDeck();
   it("New deck constains 52 cards", () => {
     for (let c in deck.cards) {
-      cy.log({ c: deck.cards[c] });
+      cy.log(JSON.stringify(deck.cards[c]));
     }
     expect(get(deck, ["cards", "length"])).to.equal(52);
   });
