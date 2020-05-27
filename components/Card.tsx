1aa81a3c9e3a24e48064924f7b3cf3728f155739
Efosa Oyegun <efosao@gmail.com>
2020-05-27 15:06:43 -0700

    added game state logic to class

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,9 +1,29 @@
 import styles from "./Card.module.css";
+import { DeckColors } from "../src/index";
 
-function Card({ value, suite }) {
+function Card({
+  color,
+  value,
+  suite,
+  onSelect = null,
+  isSelected = false,
+  isMatched = false,
+}) {
+  const cssColor = color.toLowerCase();
+  let cardName = value;
+  if (value === 1) cardName = "Ace";
+  else if (value === 11) cardName = "Jack";
+  else if (value === 12) cardName = "Queen";
+  else if (value === 13) cardName = "King";
+
+  const isSelectedClass = isSelected ? "selected" : "";
+  const isMatchedClass = isMatched ? "matched" : "";
+  const classes = `${styles.card} ${styles[cssColor]} ${styles[isSelectedClass]} ${styles[isMatchedClass]}`;
+
   return (
-    <div className={styles.card}>
-      {value}
+    <div className={classes} onClick={onSelect}>
+      {cardName}
+      <br />
       {suite}
     </div>
   );
